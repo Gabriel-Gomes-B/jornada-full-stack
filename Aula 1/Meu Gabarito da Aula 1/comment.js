b644c83a261dddaedd0a107573b9c963d56fce64
Gabriel Gomes <116276311+Gabriel-Yuki@users.noreply.github.com>
2025-02-13 00:22:09 -0300

    react
    Aula 1: material disponibilizado + estrutura do projeto

---
+++ Aula 1/Meu Gabarito da Aula 1/comment.js
@@ -0,0 +1,20 @@
+/* Comandos 
+
+    Aula 1
+
+    cd 'Aula 1/Meu Gabarito da Aula 1'
+
+    npm create vite@latest .
+
+    npm i -> instalar o node
+
+    npm run dev
+
+    npm node package manager
+
+
+
+
+    
+
+*/
---
+++ Aula 1/Meu Gabarito da Aula 1/src/App2.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+    
+const App2 = () => {
+  return <div>App2</div>;
+};
+
+export default App2
+
+// rafce -> cria uma Arrow Function
+
+// Arrow Function
+// const App2 = () =>  <h1>Olá, Mundo!</h1>;
+
+// Nomeação de Componente
+// PascalCase 
+
+// export default, posso importar com qualquer nome e sem chaves
+// export 'sem default', só posso importar entre chaves e com o nome exportando
+
+// Self closing tag
+//  
