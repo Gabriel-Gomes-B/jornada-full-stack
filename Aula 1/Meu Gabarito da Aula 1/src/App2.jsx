76dbbb5c2f73a53f235e5e11b00ab79528dc2541
Gabriel Gomes <116276311+Gabriel-Yuki@users.noreply.github.com>
2025-02-13 23:09:41 -0300

    Aula 1
    Completando a estrutura

--- Aula 1/Meu Gabarito da Aula 1/src/App2.jsx
+++ Aula 1/Meu Gabarito da Aula 1/src/App2.jsx
@@ -12,10 +12,37 @@ export default App2
 // const App2 = () =>  <h1>Olá, Mundo!</h1>;
 
 // Nomeação de Componente
-// PascalCase 
+// PascalCase
+
+// Nomeação de Variavel, Função ....
+// camelCase
 
 // export default, posso importar com qualquer nome e sem chaves
 // export 'sem default', só posso importar entre chaves e com o nome exportando
 
 // Self closing tag
-//  
+//  <Header /> 
+//  <Header><Header />
+
+// element.style {
+//   background-color: antiquewhite;
+//   padding: 20px;
+//   margin: 20px;
+//   border: solid 5px black;
+//   /* box-sizing: content-box; */
+//   width: 500px;
+// }
+
+// Nomeação de classes em css
+// Metodologia BEM
+// Blocks
+// Elements
+// Modifiers
+// bloco__elemento--modificador
+// header
+// header__link
+// header__link--small
+// item-list__header
+// nomes compostos são separados por -
+
+// Tag vazia em React se chama fragment ou fragmento
